2510b63399efa582638684dcc5238b7ec8e88e2f
Anushka <anushk0sharma@gmail.com>
2025-05-29 22:20:32 +0530

    playpause

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
@@ -20,7 +21,7 @@ const SongCard = ({ song, isPlaying, activeSong , i , data}) => {
   return (
   <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
   <div className="relative w-full h-56 group">
-    <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-30 group-hover:flex ${activeSong?.title === song?.title ? 'flex bg-black bg-opacity-80' : 'hidden'} `}>
+    <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-30 group-hover:flex ${activeSong?.attributes.name === song?.attributes.name ? 'flex bg-black bg-opacity-80' : 'hidden'} `}>
      <PlayPause 
         isPlaying= {isPlaying}
         activeSong={activeSong}
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import{ useDispatch, useSelector }from 'react-redux';
 
 import {Error, Loader, SongCard} from '../components';
