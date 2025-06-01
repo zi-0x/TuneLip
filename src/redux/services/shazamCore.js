18fe5f5aed643f5ef0bc331e3c0cb31e8a07645c
Anushka <anushk0sharma@gmail.com>
2025-06-01 20:13:11 +0530

    art details

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,16 +5,18 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
     baseQuery: fetchBaseQuery({
         baseUrl:'https://shazam-core.p.rapidapi.com/v1',
         prepareHeaders:(headers)=> {
-            headers.set('x-rapidapi-key', '3242e1cae5msh97504f23e82624bp1a90fbjsn8eb5b1ee6a70');
+            headers.set('x-rapidapi-key', 'bacd03dbe8mshd1d9ef7a40bb44cp1e0ad4jsn1feac250ba3b');
             headers.set('x-rapidapi-host', 'shazam-core.p.rapidapi.com');
             return headers;
         },
     }),
     endpoints: (builder) =>({
       getTopCharts: builder.query({ query: () => '/charts/world?country_code=DZ' }),
+      getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}` }),
     }),
   });
 
   export const { 
     useGetTopChartsQuery,
+    useGetSongDetailsQuery,
   } = shazamCoreApi;
