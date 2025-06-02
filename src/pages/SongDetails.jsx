103e1464a83a8f589ada00d6a26a7b93e948a89a
Anushka <anushk0sharma@gmail.com>
2025-06-03 01:24:40 +0530

    Final changes 2:10

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,30 +2,14 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader,Error, Loader, RelatedSongs } from '../components';
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
-import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
+import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
  const dispatch =useDispatch();
  const { songid } = useParams();
  const {activeSong, isPlaying}=useSelector((state) => state.player);
  const {data: songData, isFetching:isFetchingSongDetails}= useGetSongDetailsQuery({songid});
- const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
-
-const handlePauseClick = () => {
-      dispatch(playPause(false));
-    };
-    
-    const handlePlayClick = (song,i) => {
-      dispatch(setActiveSong({ song, data, i }));
-      dispatch(playPause(true));
-    };
-
- if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
-
-  console.log(songData);
-
-  if (error) return <Error />;
-
+ console.log(songid, songData);
 
  const songId = songData?.data[0]?.id;
  const lyricId = songData?.resources?.['shazam-songs']?.[songId]?.relationships?.lyrics?.data[0]?.id;
@@ -41,24 +25,18 @@ const handlePauseClick = () => {
         <div className="mt-5"> 
           {lyricId
             ? lyric.map((Line, i) => (
-              <p key={`lyrics-${Line}-${i}`} className="text-purple-300 text-base my-1">{Line}</p>
+              <p key={`lyrics-${Line}-${i}`} className="text-yellow-400 text-base my-1">{Line}</p>
             ))
             : 
               <p className="text-yellow-400 text-base my-1">Sorry, No lyrics found!</p>
             }
         </div>
     </div>
-     <RelatedSongs
-        data={data}
-        isPlaying={isPlaying}
-        activeSong={activeSong}
-        handlePauseClick={handlePauseClick}
-        handlePlayClick={handlePlayClick}
-        />
  </div>
  ) ;
 };
 
 export default SongDetails;
+
 
 
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,20 +5,28 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
     baseQuery: fetchBaseQuery({
         baseUrl:'https://shazam-core.p.rapidapi.com',
         prepareHeaders:(headers)=> {
-            headers.set('x-rapidapi-key', '14a8bf37d8msh93da1806169a17cp1d0c5ejsn6353f1c678d1');
+            headers.set('x-rapidapi-key', 'bef53b48f2msh122692ba49d8576p1b1a10jsn464e875f5422');
             headers.set('x-rapidapi-host', 'shazam-core.p.rapidapi.com');
             return headers;
         },
     }),
     endpoints: (builder) =>({
       getTopCharts: builder.query({ query: () => '/v1/charts/world?country_code=DZ' }),
+      getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+      getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+      getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+      getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
       getSongDetails: builder.query({ query: ({songid}) => `/v2/tracks/details?track_id=${songid}` }),
-      getSongRelated: builder.query({ query: ({songid}) => `/tracks/related?track_id=${songid}` }),
+      getSongRelated: builder.query({ query: ({songid}) => `/v1/tracks/related?track_id=${songid}` }),
     }),
   });
 
-  export const { 
-    useGetTopChartsQuery,
-    useGetSongDetailsQuery,
-    useGetSongRelatedQuery,
-  } = shazamCoreApi;
+  export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
